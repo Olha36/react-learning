ef238f5d7de09c2c0487221e5d3168c6f1065d6d
Olha36 <olhakucheruk36@gmail.com>
2024-10-13 20:28:01 +0200

    conditional rendering exercise is done

---
+++ src/components/conditionalRendering/conditionRendering.jsx
@@ -0,0 +1,37 @@
+import { useState } from 'react';
+import { IoIosCloudy, IoIosRainy, IoIosSunny } from 'react-icons/io';
+import { RiSnowyFill } from 'react-icons/ri';
+import '../../css/conditionRendering.css';
+
+export default function ConditionRendering() {
+  let [weather, setWeather] = useState('');
+  const handleClick = (e) => {
+    setWeather(e.target.innerText);
+  };
+  return (
+    <>
+      <p>What is the weather like today?</p>
+      <div className='weather-container'>
+        <div className='sunny box'>
+          <IoIosSunny />
+          <button onClick={handleClick}>Sunny</button>
+        </div>
+        <div className='cloudy box'>
+          <IoIosCloudy />
+          <button onClick={handleClick}>Cloudy</button>
+        </div>
+        <div className='rainy box'>
+          <IoIosRainy />
+          <button onClick={handleClick}>Rainy</button>
+        </div>
+        <div className='snowy box'>
+          <RiSnowyFill />
+          <button onClick={handleClick}>Snowy</button>
+        </div>
+
+     
+      </div>
+      <p>The weather is {weather}</p>
+    </>
+  );
+}
